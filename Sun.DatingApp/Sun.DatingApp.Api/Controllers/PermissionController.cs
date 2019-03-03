487e87a0ea56e8938378e939c2216def070c3f10
sunhao <2080680175@qq.com>
2019-03-03 14:39:25 +0800

    添加权限api

--- Sun.DatingApp/Sun.DatingApp.Api/Controllers/PermissionController.cs
+++ Sun.DatingApp/Sun.DatingApp.Api/Controllers/PermissionController.cs
@@ -1,5 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Sun.DatingApp.Model.Common;
+using Sun.DatingApp.Model.Permissions.Dto;
+using Sun.DatingApp.Model.Permissions.Model;
 using Sun.DatingApp.Services.Services.Permissions;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Sun.DatingApp.Api.Controllers
 {
@@ -10,6 +16,76 @@ namespace Sun.DatingApp.Api.Controllers
         public PermissionController(IPermissionService service)
         {
             _service = service;
+        }
+
+        /// <summary>
+        /// 获取权限树数据
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("getpermissions")]
+        public async Task<WebApiResult<List<PermissionTreeModel>>> GetPermissions()
+        {
+            return await _service.GetPermissions();
+        }
+
+        /// <summary>
+        /// 新建权限树节点
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <returns></returns>
+        [HttpPost("create")]
+        public async Task<WebApiResult<PermissionTreeModel>> Create(PermissionEditDto dto)
+        {
+            var result = new WebApiResult<PermissionTreeModel>();
+            if (CurrentUserId.HasValue)
+            {
+                result = await _service.Create(dto, CurrentUserId.Value);
+            }
+            else
+            {
+                result.AddError("当前用户信息获取失败");
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 修改权限节点
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <returns></returns>
+        [HttpPost("edit")]
+        public async Task<WebApiResult> Edit(PermissionEditDto dto)
+        {
+            var result = new WebApiResult();
+            if (CurrentUserId.HasValue)
+            {
+                result = await _service.Edit(dto, CurrentUserId.Value);
+            }
+            else
+            {
+                result.AddError("当前用户信息获取失败");
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 删除权限节点
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("delete")]
+        public async Task<WebApiResult> Delete(Guid id)
+        {
+            var result = new WebApiResult();
+            if (CurrentUserId.HasValue)
+            {
+                result = await _service.Delete(id, CurrentUserId.Value);
+            }
+            else
+            {
+                result.AddError("当前用户信息获取失败");
+            }
+            return result;
         }
     }
 }
