1ef5c48d15c5aa33f3d7922587389354acb7194e
sunhao <2080680175@qq.com>
2019-03-03 14:05:02 +0800

    权限的增删改

--- Sun.DatingApp/Sun.DatingApp.Services/Services/Permissions/IPermissionService.cs
+++ Sun.DatingApp/Sun.DatingApp.Services/Services/Permissions/IPermissionService.cs
@@ -1,10 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Sun.DatingApp.Model.Common;
+using Sun.DatingApp.Model.Permissions.Dto;
+using Sun.DatingApp.Model.Permissions.Model;
 
 namespace Sun.DatingApp.Services.Services.Permissions
 {
     public interface IPermissionService
     {
+        /// <summary>
+        /// 获取权限树数据
+        /// </summary>
+        /// <returns></returns>
+        Task<WebApiResult<List<PermissionTreeModel>>> GetPermissions();
+
+        /// <summary>
+        /// 新建权限树节点
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <param name="accountId"></param>
+        /// <returns></returns>
+        Task<WebApiResult<PermissionTreeModel>> Create(PermissionEditDto dto, Guid accountId);
+
+        /// <summary>
+        /// 修改权限节点
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <param name="accountId"></param>
+        /// <returns></returns>
+        Task<WebApiResult> Edit(PermissionEditDto dto, Guid accountId);
+
+        /// <summary>
+        /// 删除权限节点
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="accountId"></param>
+        /// <returns></returns>
+        Task<WebApiResult> Delete(Guid id, Guid accountId);
     }
 }
--- Sun.DatingApp/Sun.DatingApp.Services/Services/Permissions/PermissionService.cs
+++ Sun.DatingApp/Sun.DatingApp.Services/Services/Permissions/PermissionService.cs
@@ -1,8 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using Sun.DatingApp.Data.Database;
+using Sun.DatingApp.Data.Entities.System;
+using Sun.DatingApp.Model.Common;
+using Sun.DatingApp.Model.Permissions.Dto;
+using Sun.DatingApp.Model.Permissions.Model;
 using Sun.DatingApp.Services.Services.BaseServices;
 using Sun.DatingApp.Utility.CacheUtility;
 
@@ -12,6 +19,118 @@ namespace Sun.DatingApp.Services.Services.Permissions
     {
         public PermissionService(DataContext dataContext, IMapper mapper, ICacheService catchService) : base(dataContext, mapper, catchService)
         {
+
+        }
+
+        public async Task<WebApiResult<List<PermissionTreeModel>>> GetPermissions()
+        {
+            var result = new WebApiResult<List<PermissionTreeModel>>();
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                result.AddError(ex.Message);
+            }
+            return result;
+        }
+
+        public async Task<WebApiResult<PermissionTreeModel>> Create(PermissionEditDto dto, Guid accountId)
+        {
+            var result = new WebApiResult<PermissionTreeModel>();
+            try
+            {
+                var entity = new Permission
+                {
+                    Id = Guid.NewGuid(),
+                    Name = dto.Name,
+                    Code = dto.Code,
+                    Intro = dto.Intro,
+                    Icon = dto.Icon,
+                    CreatedAt = DateTime.Now,
+                    CreatedById = accountId,
+                    Deleted = false
+                };
+                _dataContext.Permissions.Add(entity);
+
+                await _dataContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                result.AddError(ex.Message);
+            }
+            return result;
+        }
+
+        public async Task<WebApiResult> Edit(PermissionEditDto dto, Guid accountId)
+        {
+            var result = new WebApiResult();
+            try
+            {
+                if (!dto.Id.HasValue)
+                {
+                    result.AddError("Id不能为空");
+                    return result;
+                }
+
+                var entity = await _dataContext.Permissions.FirstOrDefaultAsync(x => x.Id == dto.Id.Value);
+                if (entity == null)
+                {
+                    result.AddError("数据为空");
+                    return result;
+                }
+
+                var exist = await _dataContext.Permissions.AnyAsync(x => !x.Deleted && x.Id != dto.Id && x.Code == dto.Code);
+                if (exist)
+                {
+                    result.AddError("存在相同的编号");
+                    return result;
+                }
+
+                entity.Name = dto.Name;
+                entity.Code = dto.Code;
+                entity.Intro = dto.Intro;
+                entity.Icon = dto.Icon;
+                entity.UpdatedAt = DateTime.Now;
+                entity.UpdatedById = accountId;
+
+                await _dataContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                result.AddError(ex.Message);
+            }
+            return result;
+        }
+
+        public async Task<WebApiResult> Delete(Guid id, Guid accountId)
+        {
+            var result = new WebApiResult();
+            try
+            {
+                var entity = await _dataContext.Permissions.FirstOrDefaultAsync(x => x.Id == id);
+                if (entity == null)
+                {
+                    result.AddError("数据为空");
+                    return result;
+                }
+
+                entity.Deleted = true;
+                entity.DeletedAt = DateTime.Now;
+                entity.DeletedById = accountId;
+
+                await _dataContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                result.AddError(ex.Message);
+            }
+            return result;
         }
     }
 }
