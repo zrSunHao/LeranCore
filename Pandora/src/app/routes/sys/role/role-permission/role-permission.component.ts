ace23d487d115bdac02816e1662c49e250313f01
sunhao <2080680175@qq.com>
2019-03-07 08:36:15 +0800

    权限选择算法

--- Pandora/src/app/routes/sys/role/role-permission/role-permission.component.ts
+++ Pandora/src/app/routes/sys/role/role-permission/role-permission.component.ts
@@ -32,18 +32,18 @@ export class RolePermissionComponent implements OnInit {
 
   checkModule(data) {
     const checked = data.checked;
-    // console.log(data.checked);
+    console.log(data.checked);
     // tslint:disable-next-line:prefer-for-of
     for (let i = 0; i < this.listOfData.length; i++) {
-      // console.log(`${data.id}:${this.listOfData[i].id}`);
+      console.log(`${data.id}:${this.listOfData[i].id}`);
       if (this.listOfData[i].id === data.id) {
-        // console.log(data.checked);
-        this.listOfData[i].checked = !checked;
-        // console.log(this.listOfData[i].children.length);
+        console.log(data.checked);
+        this.listOfData[i].checked = checked;
+        console.log(this.listOfData[i].children.length);
         if (this.listOfData[i].children.length > 0) {
           // tslint:disable-next-line:prefer-for-of
           for (let j = 0; j < this.listOfData[i].children.length; j++) {
-            this.listOfData[i].children[j].checked = !checked;
+            this.listOfData[i].children[j].checked = checked;
           }
         }
       }
@@ -53,26 +53,36 @@ export class RolePermissionComponent implements OnInit {
 
   checkOperate(data) {
     const checked = data.checked;
-    console.log(checked);
+    // console.log(checked);
     const parentId = data.parentId;
 
+    // tslint:disable-next-line:prefer-for-of
     for (let i = 0; i < this.listOfData.length; i++) {
       // console.log(`${data.id}:${this.listOfData[i].id}`);
       if (this.listOfData[i].id === parentId) {
         let flag = true;
+        // tslint:disable-next-line:prefer-for-of
         for (let j = 0; j < this.listOfData[i].children.length; j++) {
           if (this.listOfData[i].children[j].id === data.id) {
             this.listOfData[i].children[j].checked = checked;
           }
-          if (this.listOfData[i].children[j].checked === !checked) {
+          if (this.listOfData[i].children[j].checked !== checked) {
+            // console.log(111111);
             flag = false;
           }
         }
         if (flag) {
+          // console.log(222222);
           this.listOfData[i].checked = checked;
+        } else {
+          // console.log(333333);
+          const parentChecked = this.listOfData[i].checked;
+          if (parentChecked) {
+            this.listOfData[i].checked = false;
+          }
         }
       }
     }
-    console.log(this.listOfData);
+    // console.log(this.listOfData);
   }
 }
