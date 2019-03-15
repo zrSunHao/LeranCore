3f387028b4545ccd1701681ca1ff30c517d61e47
sunhao <2080680175@qq.com>
2019-03-15 10:45:07 +0800

    头像实体类

--- Sun.DatingApp/Sun.DatingApp.Data/Entities/System/AccountAvatarFile.cs
+++ Sun.DatingApp/Sun.DatingApp.Data/Entities/System/AccountAvatarFile.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace Sun.DatingApp.Data.Entities.System
 {
-    class AccountAvatarFile
+    public class AccountAvatarFile : BaseEntity
     {
+        public string FileName { get; set; }
+
+        public string Url { get; set; }
+
+        public string FileType { get; set; }
+
+        public long FileLength { get; set; }
+
+        public Guid AccountId { get; set; }
     }
 }
