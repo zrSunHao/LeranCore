e2ba3ef40a627ff5c242990c6e0765b86ffe512d
sunhao <2080680175@qq.com>
2019-03-20 17:20:38 +0800

    添加seed

---
+++ Sun.DatingApp/Sun.DatingApp.Api/Config/DbInitializerConfigExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sun.DatingApp.Data.Database;
+
+namespace Sun.DatingApp.Api.Config
+{
+    public class DbInitializerConfigExtensions
+    {
+        public static void Initialize(DataContext context)
+        {
+            context.Database.EnsureCreated();
+
+            var test = context.Accounts.Any();
+            var see = test;
+        }
+    }
+}
--- Sun.DatingApp/Sun.DatingApp.Api/Program.cs
+++ Sun.DatingApp/Sun.DatingApp.Api/Program.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Sun.DatingApp.Api.Config;
+using Sun.DatingApp.Data.Database;
 
 namespace Sun.DatingApp.Api
 {
@@ -14,7 +17,24 @@ namespace Sun.DatingApp.Api
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<DataContext>();
+                    DbInitializerConfigExtensions.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
