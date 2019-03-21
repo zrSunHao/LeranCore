ba208fd2ee8be951a701570b9ff144264f4b1078
sunhao <2080680175@qq.com>
2019-03-21 09:55:01 +0800

    修改用户信息页面

--- Pandora/src/app/routes/sys/user/user-info/user-info.component.ts
+++ Pandora/src/app/routes/sys/user/user-info/user-info.component.ts
@@ -5,8 +5,11 @@ import {
   ChangeDetectorRef,
 } from '@angular/core';
 import { _HttpClient } from '@delon/theme';
-import { zip } from 'rxjs';
+import { zip, of } from 'rxjs';
 import { NzMessageService } from 'ng-zorro-antd';
+import { SFSchema, SFButton } from '@delon/form';
+
+const sexList = [{ label: '男', value: true }, { label: '女', value: false }];
 
 @Component({
   selector: 'app-user-info',
@@ -14,9 +17,59 @@ import { NzMessageService } from 'ng-zorro-antd';
   styleUrls: ['./user-info.component.less'],
 })
 export class UserInfoComponent implements OnInit {
-  avatar = '';
   userLoading = false;
-  user: any;
+  info: {
+    avatar: 'http://zeus-dev.oss-cn-qingdao.aliyuncs.com/a54af76a-0c2c-99ed-8694-997d7fe49b26.jpg';
+  };
+  avatar =
+    'http://zeus-dev.oss-cn-qingdao.aliyuncs.com/a54af76a-0c2c-99ed-8694-997d7fe49b26.jpg';
+
+  schema: SFSchema = {
+    properties: {
+      name: { type: 'string', title: '姓名', maxLength: 100 },
+      sex: {
+        type: 'string',
+        title: '性别',
+        enum: sexList,
+        ui: { widget: 'radio', change: console.log },
+      },
+      birthday: { type: 'string', title: '生日', format: 'date' },
+      phoneNum: {
+        type: 'string',
+        title: '电话',
+        maxLength: 11,
+        format: 'mobile',
+      },
+      qQ: { type: 'string', title: 'QQ', maxLength: 15 },
+      weChart: { type: 'string', title: '微信', maxLength: 30 },
+      occupation: { type: 'string', title: '职业', maxLength: 150 },
+      company: { type: 'string', title: '公司/学校', maxLength: 150 },
+      address: { type: 'string', title: '地址', maxLength: 150 },
+      motto: {
+        type: 'string',
+        title: '座右铭',
+        maxLength: 150,
+        ui: {
+          widget: 'textarea',
+          autosize: { minRows: 2, maxRows: 6 },
+        },
+      },
+      intro: {
+        type: 'string',
+        title: '个人简介',
+        maxLength: 300,
+        ui: {
+          widget: 'textarea',
+          autosize: { minRows: 2, maxRows: 6 },
+        },
+      },
+    },
+    required: ['name', 'sex', 'birthday', 'phoneNum'],
+  };
+
+  sfButton: SFButton = {
+    submit: '提交',
+  };
 
   constructor(
     private http: _HttpClient,
@@ -24,96 +77,13 @@ export class UserInfoComponent implements OnInit {
     private msg: NzMessageService,
   ) {}
 
-  ngOnInit(): void {
-    // zip(
-    //   this.http.get('/user/current'),
-    //   this.http.get('/geo/province'),
-    // ).subscribe(([user, province]: any) => {
-    //   this.userLoading = false;
-    //   this.user = user;
-    //   this.provinces = province;
-    //   this.choProvince(user.geographic.province.key, false);
-    //   this.cdr.detectChanges();
-    // });
-    this.user = {
-      name: null,
-      avatar:
-        'http://zeus-dev.oss-cn-qingdao.aliyuncs.com/a54af76a-0c2c-99ed-8694-997d7fe49b26.jpg',
-      userid: null,
-      email: null,
-      signature: '海纳百川，有容乃大',
-      title: '交互专家',
-      group: '蚂蚁金服－某某某事业群－某某平台部－某某技术部－UED',
-      tags: [
-        {
-          key: '0',
-          label: '很有想法的',
-        },
-        {
-          key: '1',
-          label: '专注撩妹',
-        },
-        {
-          key: '2',
-          label: '帅~',
-        },
-        {
-          key: '3',
-          label: '通吃',
-        },
-        {
-          key: '4',
-          label: '专职后端',
-        },
-        {
-          key: '5',
-          label: '海纳百川',
-        },
-      ],
-      notifyCount: 12,
-      country: 'China',
-      geographic: {
-        province: {
-          label: '上海',
-          key: '330000',
-        },
-        city: {
-          label: '市辖区',
-          key: '330100',
-        },
-      },
-      address: 'XX区XXX路 XX 号',
-      phone: '你猜-你猜你猜猜猜',
-    };
-    this.provinces = [
-      {
-        label: '上海',
-        key: '330000',
-      },
-    ];
-  }
-
-  // #region geo
-
-  provinces: any[] = [];
-  cities: any[] = [];
-
-  choProvince(pid: string, cleanCity = true) {
-    this.http.get(`/geo/${pid}`).subscribe((res: any) => {
-      this.cities = res;
-      if (cleanCity) this.user.geographic.city.key = '';
-      this.cdr.detectChanges();
-    });
-  }
-
-  // #endregion
+  ngOnInit(): void {}
 
   editAvatar() {
     console.log(1111111111);
   }
 
-  save() {
-    this.msg.success(JSON.stringify(this.user));
-    return false;
+  submit(value: any) {
+    this.msg.success(JSON.stringify(value));
   }
 }
