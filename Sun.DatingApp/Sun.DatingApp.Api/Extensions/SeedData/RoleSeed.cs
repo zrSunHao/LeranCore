864f9c206c0368aba86cdaab133edfad1418b792
sunhao <2080680175@qq.com>
2019-03-23 10:46:00 +0800

    seed完成

--- Sun.DatingApp/Sun.DatingApp.Api/Extensions/SeedData/RoleSeed.cs
+++ Sun.DatingApp/Sun.DatingApp.Api/Extensions/SeedData/RoleSeed.cs
@@ -14,6 +14,9 @@ namespace Sun.DatingApp.Api.Extensions.SeedData
             //系统超级管理员
             SystemSuperAdministrator(context);
 
+            //业务超级管理员
+            BusinessSuperAdministrator(context);
+
             context.SaveChanges();
         }
 
@@ -44,6 +47,7 @@ namespace Sun.DatingApp.Api.Extensions.SeedData
         /// 系统超级管理员赋有全部权限
         /// </summary>
         /// <param name="context"></param>
+        /// <param name="roleId"></param>
         private static void SetSystemSuperAdministratorPermission(DataContext context, Guid roleId)
         {
             var perms = context.SystemPermissions.ToList();
@@ -66,8 +70,72 @@ namespace Sun.DatingApp.Api.Extensions.SeedData
                 rPerms.Add(rPerm);
             }
 
-            context.AddRange(rPerms);
+            if (rPerms.Any())
+            {
+                context.AddRange(rPerms);
+            }
         }
 
+        /// <summary>
+        /// 业务超级管理员
+        /// </summary>
+        /// <param name="context"></param>
+        private static void BusinessSuperAdministrator(DataContext context)
+        {
+            var role = new SystemRole
+            {
+                Id = Guid.NewGuid(),
+                Name = "业务超级管理员",
+                Intro = "默认设置角色，拥有除菜单、权限、角色配置之外的所有权限",
+                Active = true,
+                Rank = 2,
+                Deleted = false,
+                CreatedAt = DateTime.Now,
+                CreatedById = Guid.Empty,
+            };
+
+            context.SystemRoles.Add(role);
+
+            SetBusinessSuperAdministratorPermission(context, role.Id);
+        }
+
+        /// <summary>
+        /// 业务超级管理员拥有除角色、菜单、权限配置之外的所有权限
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="roleId"></param>
+        private static void SetBusinessSuperAdministratorPermission(DataContext context, Guid roleId)
+        {
+            var perms = context.SystemPermissions.ToList();
+            if (perms.Any()) return;
+
+            var besidePages = new List<string>{"菜单管理","权限管理","角色管理"};
+            var besidePageIds = context.SystemPages.Where(x => !besidePages.Contains(x.Name)).Select(x => x.Id).ToList();
+
+            var rPerms = new List<SystemRolePermission>();
+            foreach (var perm in perms)
+            {
+                if (!besidePageIds.Contains(perm.Id))
+                {
+                    var rPerm = new SystemRolePermission
+                    {
+                        Id = Guid.NewGuid(),
+                        RoleId = roleId,
+                        PermissionId = perm.Id,
+                        PageId = perm.PageId,
+                        CreatedAt = DateTime.Now,
+                        CreatedById = Guid.Empty,
+                        Deleted = false
+                    };
+
+                    rPerms.Add(rPerm);
+                }
+            }
+
+            if (rPerms.Any())
+            {
+                context.AddRange(rPerms);
+            }
+        }
     }
 }
---
+++ Sun.DatingApp/Sun.DatingApp.Data/Migrations/20190323024522_EditRoleRank.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sun.DatingApp.Data.Migrations
+{
+    public partial class EditRoleRank : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SystemPermission_SystemPage_PageId",
+                table: "SystemPermission");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_SystemRolePermission_SystemRole_RoleId",
+                table: "SystemRolePermission");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SystemRolePermission_RoleId",
+                table: "SystemRolePermission");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SystemPermission_PageId",
+                table: "SystemPermission");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Rank",
+                table: "SystemRole",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Rank",
+                table: "SystemPermission",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Order",
+                table: "SystemPage",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Order",
+                table: "SystemMenu",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rank",
+                table: "SystemRole");
+
+            migrationBuilder.DropColumn(
+                name: "Rank",
+                table: "SystemPermission");
+
+            migrationBuilder.DropColumn(
+                name: "Order",
+                table: "SystemPage");
+
+            migrationBuilder.DropColumn(
+                name: "Order",
+                table: "SystemMenu");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SystemRolePermission_RoleId",
+                table: "SystemRolePermission",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SystemPermission_PageId",
+                table: "SystemPermission",
+                column: "PageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SystemPermission_SystemPage_PageId",
+                table: "SystemPermission",
+                column: "PageId",
+                principalTable: "SystemPage",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SystemRolePermission_SystemRole_RoleId",
+                table: "SystemRolePermission",
+                column: "RoleId",
+                principalTable: "SystemRole",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
